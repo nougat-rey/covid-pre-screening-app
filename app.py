b8c19bcb9e3a72116fdeb4256691adacb8a0680c
geoffn7 <geoffrey.nguyen9@gmail.com>
2021-01-19 21:07:28 -0500

    Include notes for pre-screening questions and logic

---
+++ app.py
@@ -0,0 +1,45 @@
+"""
+Questions to ask:
+
+Name:
+
+Type of Visitor (support worker, critical/palliative, government inspector, caregiver (designated))
+- no general visitors permitted
+
+Phone number/email:
+
+Name of resident visiting:
+
+2. Do you have any of the following symptoms?
+
+Fever, new or worsening cough, shortness of breath, sore throat, runny nose, sneezing or
+nasal congestion (in absence of underlying reasons for such symptoms such as seasonal allergies and post
+nasal drip), hoarse voice, difficulty swallowing, new smell or taste disorder(s), nausea/vomitting,
+diarrhea, abdominal pain, unexpected fatigue/malaise, chills and/or headache.
+
+3. In the past 14 days, while *not* wearing personal protective equipment at work or elsewhere,
+have you had close contact with anyone who has respiratory illness or confirmed or probable
+case of COVID-19?
+
+4. Have you travelled outside of Canada or had close contact with someone who has travelled in the last 14 days?
+
+5. For caregivers: You have not visited another home in outbreak or resident who is self-isolating or symptomatic in the last 14 days?
+
+6. For support workers and caregivers: You have provided proof of a negative COVID-19 test in the past week and have not since tested positive?
+
+7. For caregivers: You have read the visitor policy and reviewed the education therein and have reread the policy in the last month.
+
+8. For MLTC Inspectors: You have confirmed that you are tested every week and verbally attested to a negative COVID-19 test and have not since tested positive?
+
+2, 3, 4 visitor has passed initial screening. See other applicable questions to proceed with visit. 
+
+Yes to 2, 3 or 4 visitor has not passed screening and needs to reschedule visit. 
+
+For 5. If no, visitor has not passed screening and needs to reschedule their visit.
+
+For 6. Same as above.
+
+For 7. Same as above.
+
+For 8. If no, government inspector has not passed screening and cannot enter the home.
+"""
